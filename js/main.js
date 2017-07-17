58095939853942d976335feff3303c7e1553861f
curlyblueeagle <pr.aparna@gmail.com>
2017-07-16 20:12:19 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+/**
+ * Boostrap the game's application by instantiating the View, Model and Controller
+ * The insantiated controller automatically kickstarts all the necessary actions required make the game ready to be played.
+ */
 window.onload = () => {
 	var boardView = new BoardView();
 	var boardModel = new BoardModel();
